8eb3f6392d2d7b76b791a3e9e1a5ec49013ea5c2
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-04-16 23:06:13 +0900

    ポートフォリオトップにtitleタグ（KodoMeal)追加

--- config/views.py
+++ config/views.py
@@ -1,4 +1,16 @@
 from django.http import HttpResponse
 
 def portfolio_top(request):
-    return HttpResponse("<h1>KodoMeal<h1>")
+    html = """
+    <!DOCTYPE html>
+    <html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>KodoMeal</title>
+    </head>
+    <body>
+        <h1>KodoMeal</h1>
+    </body>
+    </html>
+    """
+    return HttpResponse(html)
