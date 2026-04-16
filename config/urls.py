d554f32bfd61ee577a6d67279fe990ca9897984a
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-04-16 22:08:22 +0900

    views.pyにHttpResponseでportfolio_top書き込む

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.portfolio_top, name='portfolio_top'),
-    path('kodomeal/', views.home, name='home'),
 ]
