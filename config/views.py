ca8bceeb0f81ce1edccef4478c33bd293e089323
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-04-17 23:01:27 +0900

    アプリトップ画面に入力欄、各ボタン追加

--- config/views.py
+++ config/views.py
@@ -33,4 +33,36 @@ def portfolio_top(request):
     return HttpResponse(html)
 
 def app_top(request):
-    return HttpResponse("KodoMeal検索画面")
+    html = """
+    <!DOCTYPE html>
+    <html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>KodoMeal 検索画面</title>
+    </head>
+    <body>
+        <h1 class="app-title">KodoMeal</h1>
+        
+        <form class="search-form">
+            <input type="text" class="keyword-input" name="keyword" placeholder="キーワード入力">
+            
+            <div class="menu-tag-group">
+                <button type="button" class="menu-tag-button">うどん</button>
+                <button type="button" class="menu-tag-button">ラーメン</button>
+                <button type="button" class="menu-tag-button">カレー</button>
+                <button type="button" class="menu-tag-button">パスタ</button>
+            </div>
+            
+            <button type="button" class="detail-serch-button">＋詳細検索</button>
+            <button type="submit" class="search-button">検索する</button>
+        </form>
+        
+        <nav class="bottom-nav">
+            <button type="button" class="nav-button">お気に入り</button>
+            <button type="button" class="nav-button">検索する</button>
+            <button type="button" class="nav-button">ログイン</button>
+        </nav>
+    </body>
+    </html>    
+    """
+    return HttpResponse(html)
