e85d64b64a72abc898fb41ffef1d1d88e96bb310
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-04-19 23:11:26 +0900

    アカウント作成画面の入力フォーム構造修正とviews.pyの画面遷移とclass名を調整

--- config/views.py
+++ config/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def portfolio_top(request): #ポートフォリオトップ画面
     html = """
@@ -23,11 +24,7 @@ def portfolio_top(request): #ポートフォリオトップ画面
             <a href="#">ER図</a>
         </div>
         
-        <div>
-            <a href="/kodomeal/">
-                <button>このアプリにアクセス</button>
-            </a>
-        </div>
+        <a href="/kodomeal/" class="app-link-button">このアプリにアクセス</a>
         
     </body>
     </html>
@@ -47,7 +44,7 @@ def app_top(request): #アプリトップ画面
         <h1 class="app-title">KodoMeal</h1>
     </div>
         
-        <form class="search-form">
+        <form method="get" class="search-form">
             <input type="text" class="keyword-input" name="keyword" placeholder="キーワード入力">
             
             <div class="menu-tag-group">
@@ -61,7 +58,7 @@ def app_top(request): #アプリトップ画面
                 
             </div>
             
-            <button type="button" class="detail-serch-button">＋詳細検索</button>
+            <button type="button" class="detail-search-button">＋詳細検索</button>
             <button type="submit" class="search-button">検索する</button>
         </form>
         
@@ -75,57 +72,5 @@ def app_top(request): #アプリトップ画面
     """
     return HttpResponse(html)
 
-def register_view(request): #アカウント登録画面
-    html = """
-    <!DOCTYPE html>
-    <html lang="ja">
-    <head>
-        <meta charset="UTF-8">
-        <title>アカウント作成</title>
-    </head>
-    <body>
-    
-        <a href="/login/" class="back-link">◀︎戻る</a>
-    
-        <h1 class="register-title">新規登録</h1>
-        
-        <p class="description">
-            以下全ての項目を入力してください
-        </p>
-        
-        <form method="post" class="register-form">
-        
-            <div class="input-group">
-                <label>ユーザー名</label><br>
-                <input type="text" class="input-field" placeholder="ユーザー名">
-                <p class="note">変更できません</p>
-            </div>
-            
-            <div class="input-group">
-                <label>メールアドレス</label><br>
-                <input type="email" class="input-field" placeholder="sample@example.com">
-            </div>
-            
-            <div class="input-group">
-                <label>パスワード</label><br>
-                <input type="password" class="input-field" placeholder="パスワード">
-            </div>
-            
-            <div class="input-group">
-                <label>パスワード(確認)</label><br>
-                <input type="password" class="input-field" placeholder="パスワード再入力">
-                <p class="note">半角英数字8文字以上20文字以内</p>
-            </div>
-            
-            <button type="submit" class="register-button">登録</button>
-            
-        </form>
-        
-        <a href="/login/" class="login-link">
-            ログインはこちら
-        </a>
-        
-    </body>
-    </html>
-    """
-    return HttpResponse(html)
+def register_view(request): #アカウント登録
+    return render(request, 'register.html')
