511be651b2c37550a44da47e05ec3aa54da5852c
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-04-20 22:18:54 +0900

    UserCreationFormにplaceholderを追加

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,7 +3,30 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'ユーザー名'
+        })
+    )
+    
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={
+            'placeholder': 'sampla@example.com'
+        })
+    )
+    
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'パスワード'
+        })
+    )
+    
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'パスワード再入力'
+        })
+    )
     
     class Meta:
         model = User
@@ -11,7 +34,7 @@ class CustomUserCreationForm(UserCreationForm):
     
 def save(self, commit=True):
     user = super().save(commit=False)
-    user.email = self.cleaned_data["email"]  #入力値をセット
+    user.email = self.cleaned_data["email"]  
     if commit:
         user.save()
     return user
