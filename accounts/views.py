51e9d42c5763bd27f2a16ed870ab43b14c852ce7
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-04-20 13:12:37 +0900

    UserCreationFormでユーザー登録処理を実装

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 
 def register_view(request):
-    form = CustomUserCreationForm()
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+
     return render(request, 'register.html', {'form':form})
 
