89b4643ce0a03d1f70c685e3fe9e46bc8a60fe02
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-04-20 13:34:38 +0900

    アカウント作成画面にメールアドレス入力フォーム追加

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,18 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    
     class Meta:
         model = User
-        fields = ("username", "password1", "password2")
+        fields = ("username", "email", "password1", "password2")
+    
+def save(self, commit=True):
+    user = super().save(commit=False)
+    user.email = self.cleaned_data["email"]  #入力値をセット
+    if commit:
+        user.save()
+    return user
+                                             
--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,6 @@ from .forms import CustomUserCreationForm
 def register_view(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
-        
         if form.is_valid():
             form.save()
             return redirect('login')
