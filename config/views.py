74147ef25fbc64f733a41ccd69a04d214200389e
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-04-20 23:15:24 +0900

    言語設定とタイムゾーンを日本に設定

--- config/views.py
+++ config/views.py
@@ -32,42 +32,4 @@ def portfolio_top(request): #ポートフォリオトップ画面
     return HttpResponse(html)
 
 def app_top(request): #アプリトップ画面
-    html = """
-    <!DOCTYPE html>
-    <html lang="ja">
-    <head>
-        <meta charset="UTF-8">
-        <title>KodoMeal 検索画面</title>
-    </head>
-    <body>
-    <div class="header">
-        <h1 class="app-title">KodoMeal</h1>
-    </div>
-        
-        <form method="get" class="search-form">
-            <input type="text" class="keyword-input" name="keyword" placeholder="キーワード入力">
-            
-            <div class="menu-tag-group">
-
-                <h2 class="menu-title">Kids Menu</h2>
-                
-                <button type="button" class="menu-tag-button">うどん</button>
-                <button type="button" class="menu-tag-button">ラーメン</button>
-                <button type="button" class="menu-tag-button">カレー</button>
-                <button type="button" class="menu-tag-button">パスタ</button>
-                
-            </div>
-            
-            <button type="button" class="detail-search-button">＋詳細検索</button>
-            <button type="submit" class="search-button">検索する</button>
-        </form>
-        
-        <nav class="bottom-nav">
-            <button type="button" class="nav-button">お気に入り</button>
-            <button type="button" class="nav-button">検索する</button>
-            <a href="/login/" class="nav-button">ログイン</a>
-        </nav>
-    </body>
-    </html>    
-    """
-    return HttpResponse(html)
+    return render(request,'app_top.html')
