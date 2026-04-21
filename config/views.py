faba529988feccfe71025f033a3c65c453ada4d7
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-04-21 12:59:56 +0900

    検索キーワードを検索結果画面に表示

--- config/views.py
+++ config/views.py
@@ -34,9 +34,9 @@ def portfolio_top(request): #ポートフォリオトップ画面
 def app_top(request): #アプリトップ画面
     return render(request,'app_top.html')
 
-def serch_result(request):
+def serch_result(request):  #検索結果画面
     keyword = request.GET.get('keyword')
     
-    return render(request, 'search_result.html', {
+    return render(request, 'search_result.html', { #キーワード受け取る
         'keyword': keyword
     })
