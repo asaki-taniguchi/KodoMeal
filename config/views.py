59495951f9ca34839d15a145ebd52abb84bb1d94
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-04-22 23:43:50 +0900

    店舗詳細に基本情報（住所、電話番号、定休日、営業時間、駐車場）追加

--- config/views.py
+++ config/views.py
@@ -57,12 +57,66 @@ def search_result(request):  #検索結果画面
     
 def store_detail(request, store_id):
     stores = [
-        {'id': 1, 'name': 'キッズカフェ ひまわり', 'menu': 'パンケーキ'},
-        {'id': 2, 'name': 'うどん屋 マルちゃん', 'menu': 'うどん'},
-        {'id': 3, 'name': 'ファミリーレストラン さくら', 'menu':'カレー'},
-        {'id': 4, 'name': 'cafe sora', 'menu':'パスタ'},
-        {'id': 5, 'name': 'おやこダイニング nico', 'menu':'オムライス'},
-        {'id': 6, 'name': '中華ダイニング 好好', 'menu':'ラーメン'},
+        {
+            'id': 1,
+            'name': 'キッズカフェ ひまわり',
+            'menu': 'パンケーキ',
+            'address': '東京都渋谷区〇〇1-1-1',
+            'phone': '03-1234-5678',
+            'holiday': '不定休',
+            'hours': '11:00〜20:00',
+            'parking': 'あり(店舗前3台)'
+        },
+        {
+            'id': 2,
+            'name': 'うどん屋 マルちゃん',
+            'menu': 'うどん',
+            'address': '東京都世田谷区〇〇2-2-2',
+            'phone': '03-9876-5432',
+            'holiday': '水曜',
+            'hours': '10:00〜18:00',
+            'parking': 'なし'
+        },
+        {
+            'id': 3,
+            'name': 'ファミリーレストラン さくら',
+            'menu':'カレー',
+            'address': '東京都杉並区〇〇3-3-3',
+            'phone': '03-2222-3333',
+            'holiday': '年中無休',
+            'hours': '10:00〜22:00',
+            'parking': 'あり(10台)'
+            },
+        {
+            'id': 4,
+            'name': 'cafe sora',
+            'menu':'パスタ',
+            'address': '東京都目黒区〇〇4-4-4',
+            'phone': '03-4444-5555',
+            'holiday': '月曜',
+            'hours': '10:00〜19:00',
+            'parking': 'なし（近隣にコインパーキングあり)'
+            },
+        {
+            'id': 5,
+            'name': 'おやこダイニング nico',
+            'menu':'オムライス',
+            'address': '東京都練馬区〇〇5-5-5',
+            'phone': '03-6666-7777',
+            'holiday': '木曜',
+            'hours': '10:00〜17:00',
+            'parking': 'あり(店舗前2台、第二駐車場3台)'
+            },
+        {
+            'id': 6,
+            'name': '中華ダイニング 好好',
+            'menu':'ラーメン',
+            'address': '東京都中野区〇〇6-6-6',
+            'phone': '03-8888-9999',
+            'holiday': '火曜',
+            'hours': '11:00〜25:00',
+            'parking': 'あり'
+            },
     ]
     
     store = next((store for store in stores if store['id'] == store_id), None)
