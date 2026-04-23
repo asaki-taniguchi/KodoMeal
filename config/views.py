7aa118a48e750a2002104e7e3df0b813b71b8d99
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-04-23 22:00:19 +0900

    投稿作成画面用viewを追加

--- config/views.py
+++ config/views.py
@@ -155,5 +155,104 @@ def store_detail(request, store_id):
         'store' : store
     })
     
+def post_create(request, store_id):
+    stores = [
+        {
+            'id': 1,
+            'name': 'キッズカフェ ひまわり',
+            'kids_menus': ['パンケーキ', 'カレー', 'パスタ'],
+            'target_age': '未就学児まで',
+            'address': '東京都渋谷区〇〇1-1-1',
+            'phone': '03-1234-5678',
+            'holiday': '不定休',
+            'hours': '11:00〜20:00',
+            'parking': 'あり(店舗前3台)',
+            'has_kids_chair': True,
+            'is_stroller_ok': True,
+            'has_diapper': True,
+            'has_kids_space': True
+        },
+        {
+            'id': 2,
+            'name': 'うどん屋 マルちゃん',
+            'kids_menus': ['キッズうどんセット'],
+            'target_age': '小学生まで',
+            'address': '東京都世田谷区〇〇2-2-2',
+            'phone': '03-9876-5432',
+            'holiday': '水曜',
+            'hours': '10:00〜18:00',
+            'parking': 'なし',
+            'has_kids_chair': True,
+            'is_stroller_ok': True,
+            'has_diapper': False,
+            'has_kids_space': False 
+        },
+        {
+            'id': 3,
+            'name': 'ファミリーレストラン さくら',
+            'kids_menus': ['カレー', 'お子様ランチ', 'ラーメン'],
+            'target_age': '小学生まで',
+            'address': '東京都杉並区〇〇3-3-3',
+            'phone': '03-2222-3333',
+            'holiday': '年中無休',
+            'hours': '10:00〜22:00',
+            'parking': 'あり(10台)',
+            'has_kids_chair': True,
+            'is_stroller_ok': True,
+            'has_diapper': True,
+            'has_kids_space': False
+            },
+        {
+            'id': 4,
+            'name': 'cafe sora',
+            'kids_menus': ['パスタ', 'カレー'],
+            'target_age': '未就学児まで',
+            'address': '東京都目黒区〇〇4-4-4',
+            'phone': '03-4444-5555',
+            'holiday': '月曜',
+            'hours': '10:00〜19:00',
+            'parking': 'なし（近隣にコインパーキングあり)',
+            'has_kids_chair': False,
+            'is_stroller_ok': True,
+            'has_diapper': False,
+            'has_kids_space': False
+            },
+        {
+            'id': 5,
+            'name': 'おやこダイニング nico',
+            'kids_menus':['オムライス', 'パンケーキ', 'ハンバーガー'],
+            'target_age': '小学生まで',
+            'address': '東京都練馬区〇〇5-5-5',
+            'phone': '03-6666-7777',
+            'holiday': '木曜',
+            'hours': '10:00〜17:00',
+            'parking': 'あり(店舗前2台、第二駐車場3台)',
+            'has_kids_chair': True,
+            'is_stroller_ok': True,
+            'has_diapper': True,
+            'has_kids_space': True,
+            },
+        {
+            'id': 6,
+            'name': '中華ダイニング 好好',
+            'kids_menus':['ラーメン', 'チャーハン', '唐揚げセット'],
+            'target_age': '小学生まで',
+            'address': '東京都中野区〇〇6-6-6',
+            'phone': '03-8888-9999',
+            'holiday': '火曜',
+            'hours': '11:00〜25:00',
+            'parking': 'あり',
+            'has_kids_chair': False,
+            'is_stroller_ok': True,
+            'has_diapper': False,
+            'has_kids_space': False
+            },
+    ]
     
+    store = next((store for store in stores if store['id'] == store_id), None)
     
+    return render(request, 'post_create.html', {
+        'store': store
+    })
+                 
+                 
