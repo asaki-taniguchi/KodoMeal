478fa0b0ca6f9055f902275c722ef33e98d6be3e
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-04-23 14:26:08 +0900

    店舗詳細画面でキッズメニューを複数表示できるように対応

--- config/views.py
+++ config/views.py
@@ -60,7 +60,7 @@ def store_detail(request, store_id):
         {
             'id': 1,
             'name': 'キッズカフェ ひまわり',
-            'menu': 'パンケーキ',
+            'kids_menu': ['パンケーキ', 'カレー', 'パスタ'],
             'address': '東京都渋谷区〇〇1-1-1',
             'phone': '03-1234-5678',
             'holiday': '不定休',
@@ -74,7 +74,7 @@ def store_detail(request, store_id):
         {
             'id': 2,
             'name': 'うどん屋 マルちゃん',
-            'menu': 'うどん',
+            'kids_menu': ['キッズうどんセット'],
             'address': '東京都世田谷区〇〇2-2-2',
             'phone': '03-9876-5432',
             'holiday': '水曜',
@@ -88,7 +88,7 @@ def store_detail(request, store_id):
         {
             'id': 3,
             'name': 'ファミリーレストラン さくら',
-            'menu':'カレー',
+            'kids_menu': ['カレー', 'お子様ランチ', 'ラーメン'],
             'address': '東京都杉並区〇〇3-3-3',
             'phone': '03-2222-3333',
             'holiday': '年中無休',
@@ -102,7 +102,7 @@ def store_detail(request, store_id):
         {
             'id': 4,
             'name': 'cafe sora',
-            'menu':'パスタ',
+            'kids_menu': ['パスタ', 'カレー'],
             'address': '東京都目黒区〇〇4-4-4',
             'phone': '03-4444-5555',
             'holiday': '月曜',
@@ -116,7 +116,7 @@ def store_detail(request, store_id):
         {
             'id': 5,
             'name': 'おやこダイニング nico',
-            'menu':'オムライス',
+            'kids_menu':['オムライス', 'パンケーキ', 'ハンバーガー'],
             'address': '東京都練馬区〇〇5-5-5',
             'phone': '03-6666-7777',
             'holiday': '木曜',
@@ -130,7 +130,7 @@ def store_detail(request, store_id):
         {
             'id': 6,
             'name': '中華ダイニング 好好',
-            'menu':'ラーメン',
+            'kids_menu':['ラーメン', 'チャーハン', '唐揚げセット'],
             'address': '東京都中野区〇〇6-6-6',
             'phone': '03-8888-9999',
             'holiday': '火曜',
