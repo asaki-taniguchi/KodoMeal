b83b76f308d94c110983bb86453423642c0695b2
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-05-03 23:15:33 +0900

    accounts/views.pyのimport先修正

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
 from posts.models import Post
-from posts.models import Store
+from stores.models import Store
 
 def register_view(request):
     if request.method == 'POST': #フォーム送信されたかのチェック
