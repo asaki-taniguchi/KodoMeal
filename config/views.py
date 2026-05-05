26f3b4b2ad15e182590695268d0d8228c848dd06
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-05-06 00:00:27 +0900

    検索結果でキッズメニュータグを取得

--- config/views.py
+++ config/views.py
@@ -45,6 +45,29 @@ def normalize_menu_name(menu_name):
     
     return 'その他'
 
+def create_menu_tags(kids_menus):
+    menu_counts = {}
+    
+    for menu in kids_menus:
+        normalized_name = normalize_menu_name(menu.menu_name)
+        
+        if normalized_name:
+            if normalized_name in menu_counts:
+                menu_counts[normalized_name] += 1
+            else:
+                menu_counts[normalized_name] = 1
+                
+    menu_tags = []
+    
+    for name, count in menu_counts.items():
+        menu_tags.append({
+            'name': name,
+            'count': count,
+        })
+        
+    return menu_tags
+                
+
 def portfolio_top(request): #ポートフォリオトップ画面
     html = """
     <!DOCTYPE html>
@@ -83,22 +106,54 @@ def search_result(request):  #検索結果画面
     keyword = request.GET.get('keyword')
     selected_menus = request.GET.getlist('menu')
     
-    stores = Store.objects.filter(
-        is_closed=False
-    ).order_by('id')
+    stores = list(
+        Store.objects.filter(
+            is_closed=False
+        ).order_by('id')
+    )
     
     if keyword :
-        stores = stores.filter(
-            name__icontains=keyword
-        )
+        stores = [
+            store for store in stores
+            if keyword in store.name
+            or any(
+                keyword in menu['name']
+                for menu in store.menu_tags
+            )      
+        ]
         
-# TODO: post_kids_menu作成後、選択メニュー検索をDB対応する
+    if selected_menus:
+        stores = [
+            store for store in stores
+            if any(
+                selected_menu == menu['name']
+                for selected_menu in selected_menus
+                for menu in store.menu_tags
+            )
+        ]
+    
+        for kids_menu in kids_menus:
+            normalized_name = normalize_menu_name(kids_menu.menu_name)
+            
+            if normalized_name in selected_menus:
+                matched_store_ids.add(kids_menu.post.store.id)
+                
+        store = stores.filter(
+            id__in=matched_store_ids
+        )
     
     for store in stores:
         store.posts_count = Post.objects.filter(
             store=store,
             is_draft=False
         ).count()
+        
+        kids_menus = PostKidsMenu.objects.filter(
+            post__store=store,
+            post__is_draft=False
+        )
+        
+        store.menu_tags = create_menu_tags(kids_menus)
         
     if request.user.is_authenticated:
         favorite_store_ids = list(
@@ -138,24 +193,7 @@ def store_detail(request, store_id):
         post__is_draft=False
     ).order_by('-created_at')
     
-    menu_counts = {}
-    
-    for menu in kids_menus:
-        normalized_name = normalize_menu_name(menu.menu_name)
-        
-        if normalized_name:
-            if normalized_name in menu_counts:
-                menu_counts[normalized_name] += 1
-            else:
-                menu_counts[normalized_name] = 1
-                
-    menu_tags = []
-    
-    for name, count in menu_counts.items():
-        menu_tags.append({
-            'name': name,
-            'count': count,
-        })
+    menu_tags = create_menu_tags(kids_menus)
     
     if request.user.is_authenticated:
         is_favorite = Favorite.objects.filter(
