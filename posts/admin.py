72a1d4f3d07ba88e4856e209ce4b6221421d4226
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-05-05 13:18:03 +0900

    PostKidsMenuを管理画面に登録

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post, Favorite, PostKidsMenu
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'store', 'menu_name', 'is_draft', 'created_at')
+    ordering = ('-created_at',)
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'store', 'created_at')
+    ordering = ('-created_at',)
+    
+@admin.register(PostKidsMenu)
+class PostKidsMenuAdmin(admin.ModelAdmin):
+    list_display = ('id','post', 'menu_name', 'quantity', 'target_age')
+    ordering = ('id',)
