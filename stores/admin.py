d35788615d5e8f9e5f88950f248a6a12d42047f5
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-05-07 23:47:26 +0900

    StoreImageを管理画面に登録

--- stores/admin.py
+++ stores/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Store
+from .models import Store, StoreImage
 
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'address', 'phone_number')
-    ordering = ('id',)
+    ordering = ('-created_at', )
+
+@admin.register(StoreImage)
+class StoreImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'store', 'image', 'created_at')
+    ordering = ('-created_at', )
