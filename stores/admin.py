0870f988b0ea018a7a0bf9ccf0090e4ce5645512
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-05-11 13:02:11 +0900

    店舗管理画面に緯度経度を表示

--- stores/admin.py
+++ stores/admin.py
@@ -3,7 +3,17 @@ from .models import Store, StoreImage
 
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'address', 'phone_number')
+    list_display = (
+        'id',
+        'name',
+        'address',
+        'phone_number',
+        'latitude',
+        'longitude',
+        'is_closed',
+        'created_at',
+        )
+    search_fields = ('name', 'address')
     ordering = ('-created_at', )
 
 @admin.register(StoreImage)
