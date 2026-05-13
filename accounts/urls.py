98abe6ddee85c87c5a0b03ecc5596a50a12b675e
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-05-13 14:26:53 +0900

    ログインをメールアドレス認証に変更

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
+    path('mypage/', views.mypage_view, name='mypage'),
+    path('mypage/posts/', views.mypage_posts_view, name='mypage_posts'),
+    path('mypage/drafts/', views.mypage_drafts_view, name='mypage_drafts'),
+    path('mypage/account/edit/', views.account_edit_view, name='account_edit'),
+]
--- config/urls.py
+++ config/urls.py
@@ -18,41 +18,28 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.urls import path, include
 
-from .forms import CustomLoginForm
 from . import views
-from accounts import views as accounts_views
 from posts import views as posts_views
 from stores import views as stores_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.portfolio_top, name='portfolio_top'),
+    
+    path('', include('accounts.urls')),
+    
     path('kodomeal/', stores_views.app_top, name='app_top'),
-    path(
-        'login/',
-        LoginView.as_view(
-            template_name='login.html',
-            authentication_form=CustomLoginForm
-            ),
-        name='login'
-        ),
-    path('register/', accounts_views.register_view, name='register'),
-    path('mypage/', accounts_views.mypage_view, name='mypage'),
-    path('mypage/posts/', accounts_views.mypage_posts_view, name='mypage_posts'),
-    path('mypage/drafts/', accounts_views.mypage_drafts_view, name='mypage_drafts'),
     path('mypage/store/register/', stores_views.store_register_view, name='store_register'),
-    path('mypage/account/edit/', accounts_views.account_edit_view, name='account_edit'),
-    path('logout/', accounts_views.logout_view, name='logout'),
     path('search/', stores_views.search_result, name='search_result'),
     path('store/<int:store_id>/', stores_views.store_detail, name='store_detail'),
+    path('store/<int:store_id>/favorite/', stores_views.toggle_favorite, name='toggle_favorite'),
+    path('favorites/', stores_views.favorite_list, name='favorite_list'),
+    
     path('store/<int:store_id>/post/create/', posts_views.post_create, name='post_create'),
     path('store/<int:store_id>/posts/', posts_views.post_list, name='post_list'),
     path('post/<int:post_id>/edit/', posts_views.post_edit, name='post_edit'),
-    path('store/<int:store_id>/favorite/', stores_views.toggle_favorite, name='toggle_favorite'),
-    path('favorites/', stores_views.favorite_list, name='favorite_list'),
 ]
 
 if settings.DEBUG:
