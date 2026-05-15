f8ab1b91b1483ec0edf095c084bf3ddbc78619f3
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-05-15 23:11:10 +0900

    新規登録時のメールアドレス重複チェックを追加

--- accounts/forms.py
+++ accounts/forms.py
@@ -21,7 +21,7 @@ class PasswordResetRequestForm(forms.Form):
     email = forms.EmailField(
         label='メールアドレス',
         widget=forms.EmailInput(attrs={
-            'peaceholder': 'sample@example.com'
+            'placeholder': 'sample@example.com'
         })
     )
 
@@ -54,6 +54,14 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+        
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('このメールアドレスはすでに登録されています。')
+        
+        return email
     
     def save(self, commit=True):
         user = super().save(commit=False)
