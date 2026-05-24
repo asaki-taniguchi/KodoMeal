8f975ed59abf98164d77014197f93907960be322
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-05-25 02:11:36 +0900

    ポートフォリオトップ画面を追加

--- config/views.py
+++ config/views.py
@@ -1,32 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def portfolio_top(request): #ポートフォリオトップ画面
-    html = """
-    <!DOCTYPE html>
-        <html lang="ja">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>KodoMeal</title>
-        </head>
-        <body>
-    
-        <h1>KodoMeal</h1> 
-        
-        <div>
-            <p>アプリのスクショ</p> 
-        </div>
-        
-        <div>
-            <a href="#">企画書</a>
-            <a href="#">画面設計図</a> 
-            <a href="#">画面遷移図</a>
-            <a href="#">ER図</a>
-        </div>
-        
-        <a href="/kodomeal/" class="app-link-button">このアプリにアクセス</a>
-        
-    </body>
-    </html>
-    """
-    return HttpResponse(html)
+    return render(request, 'portfolio_top.html')
