e609d77f2db937023cccdad4b37744d5d12959e6
谷口雄紀 <taniguchiyuki@taniguchiyuukinoMacBook-Air.local>
2026-05-27 22:53:59 +0900

    登録画面のパスワードバリデーションを修正

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,3 +1,4 @@
+import re
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -62,6 +63,26 @@ class CustomUserCreationForm(UserCreationForm):
             raise forms.ValidationError('このメールアドレスはすでに登録されています。')
         
         return email
+    
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+
+        if len(password1) < 8:
+            raise forms.ValidationError('パスワードは8文字以上で入力してください。')
+
+        if len(password1) > 20:
+            raise forms.ValidationError('パスワードは20文字以内で入力してください。')
+
+        if not re.search(r'[A-Za-z]', password1):
+            raise forms.ValidationError('パスワードには英字を1文字以上含めてください。')
+
+        if not re.search(r'\d', password1):
+            raise forms.ValidationError('パスワードには数字を1文字以上含めてください。')
+
+        if password1 in ['password', 'password123', 'test12345', 'admin1234', 'qwerty123']:
+            raise forms.ValidationError('このパスワードは一般的すぎます。')
+
+        return password1
     
     def save(self, commit=True):
         user = super().save(commit=False)
